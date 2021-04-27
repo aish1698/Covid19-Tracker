adf0bec87e927b63a8f6245c74edcf4cf23de7c8
aish1698 <paniaishwarya98@gmail.com>
2021-04-28 01:07:25 +0530

    coronatracker

---
+++ covid19Tracker/src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { CoronaService } from './services/corona.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  countries:any;
+  country:any;
+  confirmed: Number | undefined;
+  recovered: Number | undefined;
+  deaths: Number | undefined;
+
+  constructor(private corona:CoronaService){}
+
+
+  ngOnInit(){
+    this.corona.getCountries().subscribe((data)=>{
+
+      console.log(data);
+      this.countries=data;
+    })
+
+  }
+  getCoronaData(){
+    this.corona.getCoronaRealtimeData(this.country).subscribe((data)=>
+    {
+      console.log(data);
+      var index=data.length-1;
+      console.log(index);
+      this.confirmed=data[index].Confirmed;
+      this.recovered=data[index].Recovered;
+      this.deaths=data[index].Deaths;
+
+    })
+  }
+  getCountry(country:any){
+    this.country=country
+
+
+  }
+}
